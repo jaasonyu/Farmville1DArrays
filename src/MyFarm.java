dec9df2c5f3286f83c067be9a9b775bcb4e7f2cf
killiam-cs <charlotte_killiam@milton.edu>
2025-04-01 11:13:52 -0400

    small edits

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -29,11 +29,7 @@ public class MyFarm {
     }
 
     public void displayPlots() {
-        // use the Plot class's printInfo() method to display info for each plot
-    }
-
-    public void totalPlants() {
-        // how many total plants are there in the row?
+        // use the printPlotInfo() method in Plot.java to print all information for each plot in the row
 
     }
 
@@ -42,19 +38,17 @@ public class MyFarm {
 
     }
 
-    public void printRowInfo() {
-        // use the printPlotInfo() method in Plot.java to print all information for each plot in the row
+    public void totalPlants() {
+        // how many total plants are there in the row?
 
     }
-
-
 
     public void tomatoLocations() {
         // print indexes of all tomato plants
         // call printPlantNames() method to make sure you're getting the correct indexes
 
-
         // Note: this one needs a for loop, NOT a for each loop
+
     }
 
     public void totalCarrots() {
@@ -68,29 +62,33 @@ public class MyFarm {
     }
 
     public void numberOfCarrotPlots() {
-        // how many plots have carrots on them
+        // how many plots have carrots on them?
 
     }
 
     public void numberOfEmptyPlots() {
-        // how many plots are empty
+        // how many plots are empty?
 
     }
 
     public void everyOtherNeedsWater(){
         // change the value of needsWater to be true for even numbered indexes and false for odd numbered indexes
+
     }
 
     public void plantWithMaxNumber() {
         // which plant type has the most total plants?
+
     }
 
     public void plantWithMinNumber() {
         // which plant type has the least total plants (not counting empty plots)?
+
     }
 
     public void greaterThan10() {
         // how many plots have more than 10 plants in the plot?
+
     }
 
     public void plantWithMaxNumberNeedsWater(){
@@ -104,10 +102,12 @@ public class MyFarm {
     public void greatestDifferenceNumberBetweenAnyPlants() {
         // of every pair of adjacent plots, which two have the greatest difference in plant numbers?
         // what is that difference?
+
     }
 
     public void numberOfCarrotPlotsNextToLettuce() {
         // how many carrot plots share a border with a lettuce plot?
+
     }
 
     /***
@@ -115,6 +115,7 @@ public class MyFarm {
      */
     public void greatestDifferenceOfPlantNumbers() {
         // what is the largest difference in plant numbers between two plants of the same type?
+
     }
 
 }
